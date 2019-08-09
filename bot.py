fa156e17cd54e0617212f7e420aece65185e0500
Anand P S <49161738+anandvfcvijay@users.noreply.github.com>
2019-08-09 11:14:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,14 +28,14 @@ bot = telegram.Bot(token=TOKEN)
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-def Start(bot, update):
+def Start(bot, update, context):
 	update.effective_message.reply_text("Hai!")
 
-def Reply(bot, update):
+def Reply(bot, update, context):
 	msg = update.effective_message.text
 	update.effective_message.reply_text(msg)
 
-def SendToCreator(bot, update):
+def SendToCreator(bot, update, context):
 	name = update.effective_message.from_user.first_name
 	msg = update.effective_message
 	text = update.effective_message.text
@@ -49,7 +49,7 @@ def SendToCreator(bot, update):
 	bot.sendMessage(CreatorID, message, parse_mode=ParseMode.MARKDOWN)
 	update.effective_message.reply_text("Message was sent!")
 
-def Log(bot, update):
+def Log(bot, update, context):
 	message = update.effective_message
 	eventdict = message.to_dict()
 	jsondump = json.dumps(eventdict, indent=4)
